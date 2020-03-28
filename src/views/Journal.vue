1796f3118ab10378ae9c7d396a936128d7f44e84
zct8002 <zct8002@rit.edu>
2020-03-28 17:01:51 -0400

    Journal tabs live, not in correct place

--- src/views/Journal.vue
+++ src/views/Journal.vue
@@ -1,7 +1,33 @@
 <template>
     <v-container>
         <v-layout>
-            <h1>Journal</h1>
+            <h1>Journal<br></h1>
+            <v-tabs
+            color="green"
+            grow
+            slider-color="cyan"
+            >
+            <v-tab ripple>
+                Color
+            </v-tab>
+            <v-tab ripple>
+                Word
+            </v-tab>
+            <v-tab-item>
+                <v-card flat>
+                    <v-card-text>
+                        contents
+                    </v-card-text>
+                </v-card>
+            </v-tab-item>
+            <v-tab-item>
+                <v-card flat>
+                    <v-card-text>
+                        contents
+                    </v-card-text>
+                </v-card>
+            </v-tab-item>
+            </v-tabs>
         </v-layout>
     </v-container>
 </template>
