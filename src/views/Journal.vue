7108421e3cd02f6263f3caa7a2ed16c9b984ed09
Zach Tucker <zct8002@rit.edu>
2020-03-28 23:21:25 -0400

    Revert "Update Journal.vue"
    This reverts commit eb01ffede7a0a12431433971830c22921aae9886.

--- src/views/Journal.vue
+++ src/views/Journal.vue
@@ -19,18 +19,9 @@
             </v-tab>
             <v-tab-item>
                 <v-card flat>
-                    <v-card-actions>
-                        <v-col  cols="12">
-                            <v-row>
-                        <v-btn width="50%" color="red"></v-btn>
-                        <v-btn width="50%" color="yellow"></v-btn>
-                            </v-row>
-                        <v-row>
-                        <v-btn width="50%" color="blue"></v-btn>
-                        <v-btn width="50%" color="green"></v-btn>
-                        </v-row>
-                        </v-col>
-                    </v-card-actions>
+                    <v-card-text>
+                        contents
+                    </v-card-text>
                 </v-card>
             </v-tab-item>
             <v-tab-item>
@@ -43,9 +34,9 @@
             </v-tabs>
                 </v-col>
             <v-col cols="12">
-            <v-btn width="33%" large outlined to="/">Back</v-btn>
-            <v-btn width="33%" large outlined to="/">Skip Journal</v-btn>
-            <v-btn width="33%" large outlined to="/">Next</v-btn>
+            <v-btn large outlined to="/">Home</v-btn>
+            <v-btn large outlined to="/">Skip</v-btn>
+            <v-btn large outlined to="/">Next</v-btn>
             </v-col>
             </v-row>
         </v-layout>
@@ -59,6 +50,5 @@
 </script>
 
 <style scoped>
-
 
 </style>
