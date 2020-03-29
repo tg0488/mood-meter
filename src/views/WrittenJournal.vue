585fcda9d6c09a9da465b5c52e9ab91cef7582d6
tg0488 <taylor.grant0488@gmail.com>
2020-03-29 16:19:11 -0400

    updates to buttons

--- src/views/WrittenJournal.vue
+++ src/views/WrittenJournal.vue
@@ -9,8 +9,15 @@
           <v-textarea outlined rows="10" color=""></v-textarea>
         </v-col>
         <v-col cols="12">
-            <v-btn width="50%" to="/Journal">Back</v-btn>
-            <v-btn width="50%" to="/">Next</v-btn>
+          <v-row>
+            <v-col cols="6">
+              <v-btn class="direction_buttons_left" large outlined to="/Journal">Back</v-btn>
+            </v-col>
+            <!--<v-btn width="33%" large outlined to="/">Skip Journal</v-btn>-->
+            <v-col cols="6">
+              <v-btn class="direction_buttons_right" large outlined to="/">Next</v-btn>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-layout>
@@ -23,4 +30,15 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+  .direction_buttons_left{
+    width: 85%;
+    margin-left: 10%;
+    margin-right: 5%;
+  }
+  .direction_buttons_right{
+    width: 85%;
+    margin-left: 5%;
+    margin-right: 10%;
+  }
+</style>
