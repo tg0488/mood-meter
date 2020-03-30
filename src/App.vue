4c84260cc12a74141ec68cd94290a5ea9c39bf34
tg0488 <taylor.grant0488@gmail.com>
2020-03-29 23:10:20 -0400

    updated title bar and site flow

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <v-app>
-    <main>
+    <v-app-bar
+            app
+            dark
+            src="https://i.picsum.photos/id/168/1920/1080.jpg">
+        <v-toolbar-title>Insert Title Here</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <template v-slot:extension>
+          <v-tabs dark>
+            <v-tab to="/">Journal</v-tab>
+            <v-tab to="/PersonalLog">Personal Log</v-tab>
+            <v-tab to="/teamHome">Team Home</v-tab>
+          </v-tabs>
+        </template>
+    </v-app-bar>
+    <v-content>
       <router-view></router-view>
-    </main>
+    </v-content>
   </v-app>
 </template>
 
