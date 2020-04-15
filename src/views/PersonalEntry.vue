0e05b86a6b32cb6df15343662c44df053e011a2c
Dan Murray <dqm8494@rit.edu>
2020-04-14 21:27:41 -0400

    .

--- src/views/PersonalEntry.vue
+++ src/views/PersonalEntry.vue
@@ -71,7 +71,37 @@
                     month = "January";
                     break;
                     case '02':
-                    month = "January";
+                    month = "February";
+                    break;
+                    case '03':
+                    month = "March";
+                    break;
+                    case '04':
+                    month = "April";
+                    break;
+                    case '05':
+                    month = "May";
+                    break;
+                    case '06':
+                    month = "June";
+                    break;
+                    case '07':
+                    month = "July";
+                    break;
+                    case '08':
+                    month = "August";
+                    break;
+                    case '09':
+                    month = "September";
+                    break;
+                    case '10':
+                    month = "October";
+                    break;
+                    case '11':
+                    month = "November";
+                    break;
+                    case '12':
+                    month = "December";
                     break;
                 }
                 return (month + " " + day)
